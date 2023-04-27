8588ce6d10b722dfae39eb258164cbf13b07c515
andyching168 <andyching168@gmail.com>
2023-04-27 18:19:26 +0800

    成功實裝個人攝影機跟隨功能

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -3,43 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
 
-public class CameraFollow : NetworkBehaviour
+public class CameraFollow : MonoBehaviour
 {
     public Transform target;
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
 
+    private NetworkRunner networkRunner;
+
     private void Start()
     {
-        offset = new Vector3(0f, 5f, -10f); // 修改这里的值以更改摄像机的初始位置
+        offset = new Vector3(0f, 5f, -10f);
+        networkRunner = FindObjectOfType<NetworkRunner>();
         StartCoroutine(FindPlayer());
     }
-
 
     private IEnumerator FindPlayer()
     {
         while (target == null)
         {
-            // 找到當前玩家
             GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-            print("1");
             foreach (GameObject player in players)
             {
                 NetworkObject networkObject = player.GetComponent<NetworkObject>();
-                
-                if (networkObject != null && networkObject.HasStateAuthority)
+
+                if (networkObject != null && networkObject.InputAuthority == networkRunner.LocalPlayer)
                 {
-                    
                     target = player.transform;
-                    
                     break;
                 }
             }
 
-            // 等待一段時間再次檢查
             yield return new WaitForSeconds(1f);
         }
-        print("ok");
     }
 
     private void FixedUpdate()
@@ -49,13 +45,8 @@ public class CameraFollow : NetworkBehaviour
             return;
         }
 
-        // 計算新的攝影機位置
-        
         Vector3 desiredPosition = target.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
-
-        // 讓攝影機不朝向目標
-        //transform.LookAt(target);
     }
 }
