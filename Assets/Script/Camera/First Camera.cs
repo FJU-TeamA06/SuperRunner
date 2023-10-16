1804bb1c53f8ea9b0378261a4ad2577d9171ec1e
andyching168 <andyching168@gmail.com>
2023-10-16 12:48:52 +0800

    FPS模式遊戲邏輯分開

--- Assets/Script/Camera/First Camera.cs
+++ Assets/Script/Camera/First Camera.cs
@@ -1,69 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using Fusion;
 
 public class FirstCamera : MonoBehaviour
 {
+    Vector2 viewInput;
+    float cameraRotationX = 0;
+    float cameraRotationY = 0;
+    NetworkCharacterControllerPrototype networkCharacterControllerPrototypeCustom;
     private Vector3 offset;
-    private NetworkRunner networkRunner;
-    public Transform target;
+    public Transform cameraAnchorPoint; 
     public float sensitivity = 1.0f; // 控制視角靈敏度
     public Transform playerBody; // 用於旋轉角色身體的變換
     //public Transform player; // 玩家的Transform
-    private IEnumerator FindPlayer()
-    {
-        while (target == null)
-        {
-            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-            foreach (GameObject player in players)
-            {
-                NetworkObject networkObject = player.GetComponent<NetworkObject>();
-
-                if (networkObject != null && networkObject.InputAuthority == networkRunner.LocalPlayer)
-                {
-                    target = player.transform;
-                    break;
-                }
-            }
-
-            yield return new WaitForSeconds(1f);
-        }
-    }
-
     private float rotationX = 0;
     private float rotationY = 0;
-    private void Start()
+    Camera localCamera;
+
+    
+    private void Awake()
     {
-        if (SceneManager.GetActiveScene().name == "FPS") // 指定場景的名稱
-        {
-            Cursor.lockState = CursorLockMode.Locked; 
-            networkRunner = FindObjectOfType<NetworkRunner>();
-            StartCoroutine(FindPlayer());
-            // 鎖定滑鼠游標到遊戲視窗內
-            //offset = transform.position - playerBody.transform.position;
-            //transform.localRotation = Quaternion.identity;
-            //transform.localRotation = Quaternion.Euler(0f, 90f, 0f);
-        }
-        else
-        {
-            // 在其他場景中禁用這些功能
-            // 例如：playerObject.SetActive(false);
-        }
+        localCamera = GetComponent<Camera>();
+    }
+    void Start()
+    {
+        if(localCamera.enabled)
+                localCamera.transform.parent=null;
+                Cursor.lockState = CursorLockMode.Locked; 
         
     }
 
-    private void FixedUpdate()
+    void LateUpdate()
     {
+        if(cameraAnchorPoint==null)
+            return;
+        if(!localCamera.enabled)
+            return;
+        localCamera.transform.position = cameraAnchorPoint.position;
+        cameraRotationX+=viewInput.y*Time.deltaTime;
+        cameraRotationX=Mathf.Clamp(cameraRotationX,-90,90);
+        cameraRotationY+=viewInput.x*Time.deltaTime;
+        localCamera.transform.rotation=Quaternion.Euler(cameraRotationX,cameraRotationY,0);
         // 偵測是否按住滑鼠右鍵（您可以更改成其他按鍵）
         //if (Input.GetMouseButton(1))
         //{
-            if (target == null)
-            {
-                return;
-            }
-
+/*
             // 獲取滑鼠輸入
             float mouseX = Input.GetAxis("Mouse X") * sensitivity;
             float mouseY = Input.GetAxis("Mouse Y") * sensitivity;
@@ -75,6 +57,13 @@ public class FirstCamera : MonoBehaviour
             transform.localRotation = Quaternion.Euler(rotationX, 1, 0); // 上下旋轉
             playerBody.Rotate(Vector3.up * mouseX*1); // 左右旋轉
         //}
+*/
+
+
+    }
+    public void SetViewInputVector(Vector2 viewInput)
+    {
+        this.viewInput=viewInput;
     }
   
 }
---
+++ Assets/Script/ForFPS/BasicSpawnerFPS.cs
@@ -0,0 +1,199 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+using Fusion.Sockets;
+using System;
+using UnityEngine.SceneManagement;
+
+public class BasicSpawnerFPS : MonoBehaviour, INetworkRunnerCallbacks
+{
+    [SerializeField]
+    private NetworkRunner networkRunner = null;
+
+    [SerializeField]
+    private NetworkPrefabRef playerPrefab;
+    
+    public PlayerController PlayerController;
+    private Dictionary<PlayerRef, NetworkObject> playerList = new Dictionary<PlayerRef, NetworkObject>();
+    [SerializeField]
+    public GameMode gameMode;
+    public int playerNumber; //
+    public int levelIndex; // 使用int类型的levelIndex来表示当前选中的关卡
+    private Dictionary<int, Dictionary<int, Vector3>> spawnPositions;
+    private bool isServer = false;
+
+    private void Start()
+    {
+        //PlayerController playerController = GetComponent<PlayerController>();
+        //playerController.SetBasicSpawner(this);   //傳遞
+
+        spawnPositions = new Dictionary<int, Dictionary<int, Vector3>>()
+        {
+            { 1, new Dictionary<int, Vector3>()
+                {
+                    { 1, new Vector3(0, 2, 0) },
+                    { 2, new Vector3(0, 2, 2F) }
+                }
+            },
+            { 2, new Dictionary<int, Vector3>()
+                {
+                    { 1, new Vector3(0, 2, 200) },
+                    { 2, new Vector3(0, 2, 202F) }
+                    // 在這裡添加第二個關卡的初始位置設定
+                }
+            },
+        };
+        if (IsServerMode())
+        {
+            StartServer(gameMode);
+        }
+        //StartGame(gameMode);
+    }
+
+  
+
+
+    public async void StartServer(GameMode mode)
+    {
+        await networkRunner.StartGame(new StartGameArgs()
+        {
+            GameMode = mode,
+            SessionName = "Fusion Room",
+            Scene = SceneManager.GetActiveScene().buildIndex,
+            SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
+        });
+    }
+    public async void StartGame(GameMode mode, int selectedLevel, int PlayerNum)
+    {//初始化房間和自動配對
+        if (playerPrefab == null)
+        {
+            Debug.LogError("Player prefab not set.");
+            return;
+        }
+        levelIndex = selectedLevel;
+        playerNumber = PlayerNum;
+        networkRunner.ProvideInput = true;  //tell runner we provide input
+        //print("OK");
+        await networkRunner.StartGame(new StartGameArgs()
+        {
+            GameMode = mode,
+            SessionName = "Fusion Room",
+            Scene = SceneManager.GetActiveScene().buildIndex,
+            SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
+        });
+    }
+
+   
+
+    public Vector3 GetSpawnPosition(int levelIndex, int playerNumber)
+    {
+        if (spawnPositions.TryGetValue(levelIndex, out Dictionary<int, Vector3> levelSpawnPositions))
+        {
+            if (levelSpawnPositions.TryGetValue(playerNumber, out Vector3 playerSpawnPosition))
+            {
+                return playerSpawnPosition;
+            }
+        }
+        // 如果找不到則返回默認重生位置
+        return Vector3.zero;
+    }
+
+    private bool IsServerMode()
+    {
+        // 判斷是否為 Server 模式的程式碼...
+        if (gameMode == GameMode.Server)
+        {
+            isServer = true;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    {
+        Vector3 spawnPosition;
+        if (spawnPositions.TryGetValue(levelIndex, out Dictionary<int, Vector3> levelSpawnPositions))
+        {
+            if (levelSpawnPositions.TryGetValue(playerNumber, out Vector3 playerSpawnPosition))
+            {
+                //Debug.Log(playerNumber);//P1 or P2
+                //Debug.Log(playerSpawnPosition);//P1 or P2
+                NetworkObject networkPlayerObject = runner.Spawn(playerPrefab, playerSpawnPosition, Quaternion.identity, player);
+                playerList.Add(player, networkPlayerObject);
+            }
+            else
+            {
+                // 处理无法找到 PlayerNum 的逻辑
+            }
+        }
+        else
+        {
+            if (isServer)
+            { 
+                NetworkObject networkPlayerObject = runner.Spawn(playerPrefab, new Vector3(0, 2, 0), Quaternion.identity, player);
+                playerList.Add(player, networkPlayerObject);
+            }
+            else
+            {
+                Debug.LogError("Spawn position not found for the selected level index.");
+            }
+
+        }
+
+    }
+
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+    {
+        if (playerList.TryGetValue(player, out NetworkObject networkObject))
+        {
+            runner.Despawn(networkObject);
+            playerList.Remove(player);
+        }
+    }
+
+    public void OnInput(NetworkRunner runner, NetworkInput input)
+    {
+        var data = new NetworkInputData();
+
+        if (Input.GetKey(KeyCode.A))
+            data.movementInput += Vector3.left;
+        if (Input.GetKey(KeyCode.D))
+            data.movementInput += Vector3.right;
+        if (Input.GetKey(KeyCode.W))
+            data.movementInput += Vector3.forward;
+        if (Input.GetKey(KeyCode.S))
+            data.movementInput += Vector3.back;
+
+        /*if (Input.GetKey(KeyCode.UpArrow))
+            data.movementInput += Vector3.right;
+        if (Input.GetKey(KeyCode.DownArrow))
+            data.movementInput += Vector3.left;
+        if (Input.GetKey(KeyCode.LeftArrow))
+            data.movementInput += Vector3.forward;
+        if (Input.GetKey(KeyCode.RightArrow))
+            data.movementInput += Vector3.back;*/
+
+        data.buttons.Set(InputButtons.JUMP, Input.GetKey(KeyCode.Space));
+        data.buttons.Set(InputButtons.FIRE, Input.GetKey(KeyCode.Mouse0));
+        input.Set(data);
+    }
+
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
+    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) { }
+    public void OnConnectedToServer(NetworkRunner runner) { }
+    public void OnDisconnectedFromServer(NetworkRunner runner) { }
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { }
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) { }
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) { }
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ArraySegment<byte> data) { }
+    public void OnSceneLoadDone(NetworkRunner runner) { }
+    public void OnSceneLoadStart(NetworkRunner runner) { }
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
+}
