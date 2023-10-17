c78bc8ec7dace75bcefeab289cc97ba4c44382a2
natalie890132 <115782622+natalie890132@users.noreply.github.com>
2023-10-17 13:21:41 +0800

    背景音確定

---
+++ Assets/Script/AudioController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Play(AudioSource audioData)
+    {
+        if (audioData != null)
+        {
+            audioData.Play(0);
+        }
+    }
+}
