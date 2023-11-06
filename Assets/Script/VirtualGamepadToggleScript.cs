281f8f602a5c89dfe971bdecdb7d0d7620561718
andyching168 <andyching168@gmail.com>
2023-11-06 11:03:14 +0800

    虛擬搖桿

---
+++ Assets/Script/VirtualGamepadToggleScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VirtualGamepadToggleScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject VirtualGamepad;
+    void Start()
+    {
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(OnButtonClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnButtonClick()
+    {
+        VirtualGamepad.SetActive(!VirtualGamepad.activeSelf);
+    }
+}
