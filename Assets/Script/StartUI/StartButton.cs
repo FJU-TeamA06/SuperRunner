aea4d30a1337aa509909f8318cf39799d55817ab
andyching168 <andyching168@gmail.com>
2023-11-20 12:01:49 +0800

    小修正

--- Assets/Script/StartUI/StartButton.cs
+++ Assets/Script/StartUI/StartButton.cs
@@ -10,12 +10,13 @@ public class StartButton : MonoBehaviour
     //public Button okButton;
     public GameObject levelPanel; 
     public GameObject SettingButton;
-
+    public GameObject LeaderboardButton;
     
 
     public void OnButtonClick()
     {
         levelPanel.SetActive(true); 
         SettingButton.SetActive(false);
+        LeaderboardButton.SetActive(false);
     }
 }
